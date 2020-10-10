40fd04c113ba4ceee6e4953d05a7b739854921d1
Leslie <gaozedi@gmail.com>
2020-10-10 17:28:41 +0100

    Initialize

---
+++ API/Program.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Persistence;
+
+namespace API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //args are aruguments specified in command line
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<DataContext>();
+                    //Applies any pending migrations for the context to the database,
+                    //will create the database if it dose already not exist.
+                    context.Database.Migrate();
+                }
+                catch (System.Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occured during migration.");
+                    throw;
+                }
+            }
+            host.Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
---
+++ Models/Value.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Models
+{
+    public class Value
+    {
+       public int Id { get; set; }
+       public string Name { get; set; }
+    }
+}
